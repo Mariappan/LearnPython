f9699ff1b1a5de5a602f54feaa0a32ddfadde611
Mariappan <mariappanr@outlook.com>
2014-07-31 06:24:49 -0700

    Added string concatnation using print

---
+++ 5_strings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def main():
+    print ("-----------------\n  print() Usage\n-----------------")
+    print ("In print(), ',' add a space between strings. '+' - just concatanate")
+    print ()
+
+    print ('print ("This" , "is" , "a" , "test");')
+    print ("This" , "is" , "a" , "test\n")
+
+    print ('print ("This", "is", "a", "test");')
+    print ("This", "is", "a", "test\n")
+
+    print ('print ("This" + "is" + "another" + "test");')
+    print ("This" + "is" + "another" + "test\n")
+
+if __name__ == '__main__':
+    main()
